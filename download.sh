a1e2cb5c2d3624c0cb0af93e01785dcc6f299011
zinuok <zinuok@kaist.ac.kr>
2024-05-21 20:42:01 +0900

    downdload script added

---
+++ download.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# circle
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/circle/circle.bag
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/circle/circle_fast.bag
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/circle/circle_head.bag
+
+# infinite
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/infinite/infinite.bag
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/infinite/infinite_fast.bag
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/infinite/infinite_head.bag
+
+# square
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/square/square.bag
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/square/square_fast.bag
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/square/square_head.bag
+
+# rotation
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/rotation/rotation.bag
+wget https://urserver.kaist.ac.kr/publicdata/KAIST_VIO_Dataset/rotation/rotation_fast.bag
